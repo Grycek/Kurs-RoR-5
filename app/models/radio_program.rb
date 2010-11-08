45c2d2c2d3f46186c1e872c56d9720765bd3c1d1
Grycek <adam.grycner@wp.pl>
2010-11-08 22:46:11 +0100

    Model test

--- app/models/radio_program.rb
+++ app/models/radio_program.rb
@@ -1,3 +1,4 @@
 class RadioProgram < ActiveRecord::Base
     belongs_to :user
+    validates :user_id, :presence => true
 end
--- test/unit/radio_program_test.rb
+++ test/unit/radio_program_test.rb
@@ -5,4 +5,10 @@ class RadioProgramTest < ActiveSupport::TestCase
   test "the truth" do
     assert true
   end
+  
+  #Przyklad testu modelu
+  test "should not save RadioProgram without an author id" do
+    radioprogram = RadioProgram.new(:announcement => 'Audycja 1', :name => 'Tytul audycji', :playlist => 'Cos - costam')
+    assert !radioprogram.save, "Saved the radioprogram without an author id "
+  end
 end
